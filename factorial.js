c992ec27ddd8befc60682d85ee849775140f4c56
Md Sulayman Hosen Abir <73769735+Sh75cr7@users.noreply.github.com>
2021-08-23 21:52:53 +0600

    java apply

---
+++ factorial.js
@@ -0,0 +1,19 @@
+/*
+3! = 3 X 2 X 1
+4! = 4 X 3 X 2 X 1
+5! = 5 X 4 X 3 X 2 X 1
+6! = 6 X 5 X 4 X 3 X 2 X 1
+7! = 7 X 6 X 5 X 4 X 3 X 2 X 1
+*/
+var factorial = 1;
+for (var i = 1; i <= 7; i++) {
+    factorial = factorial * i;
+}
+console.log(factorial);
+
+
+var factorial=1;
+for (var i=1; i<=9; i++ ){
+    factorial= factorial*1;
+}
+console.log(factorial);
---
+++ odd-even.js
@@ -0,0 +1,52 @@
+// const number = 4;
+// const remainder = number % 2;
+// console.log(remainder == 0); 
+// console.log(number % 2 == 0); 
+
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
+}
+
+const myNumber = 1641;
+const isMyNumberEven = isEven(myNumber);
+console.log('is my number even ', isMyNumberEven)
+
+const herNumber = 1892;
+const isHerNumberEven = isEven(herNumber);
+console.log('is her number even', isHerNumberEven);
+
+
+// is odd function
+function isOdd(number) {
+    if (number % 2 != 0) {
+        return true;
+    }
+    return false;
+}
+
+const isMyNumberOdd = isOdd(myNumber);
+console.log('is my number odd', isMyNumberOdd);
+
+const isHerNumberOdd = isOdd(herNumber);
+console.log('is her number odd', isHerNumberOdd);
+
+
+///is pactics//
+
+function isEven(number){
+    if(number % 2 ==0){
+        return true;
+    }
+    return false;
+}
+const myNumber=2000;
+const isMyNumberEven= isEven(myNumber);
+console.log('is my number' , isMyNumberEven);
+
+const herNumber=4543;
+const isHerNumberOdd= isEven(herNumber);
+console.log('is her number', isHerNumberOdd);
